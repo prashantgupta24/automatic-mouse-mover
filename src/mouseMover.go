9947aac5c547d490fe38ff5343bdb2c98be08694
Prashant Gupta <prashantgupta@us.ibm.com>
2019-03-08 18:48:08 -0800

    adding system tray feature

--- src/mouseMover.go
+++ src/mouseMover.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/getlantern/systray"
+	"github.com/getlantern/systray/example/icon"
 	"github.com/go-vgo/robotgo"
 )
 
 const (
-	timeToCheck = 3
+	timeToCheck = 5
 )
 
 type mousePos struct {
@@ -16,7 +18,9 @@ type mousePos struct {
 	mouseY int
 }
 
-func moveMouse(comm chan struct{}, quit chan struct{}) {
+func moveMouse(comm, quit chan struct{}) {
+	quitMouseClickHandler := make(chan struct{})
+	go isMouseClicked(comm, quitMouseClickHandler)
 	ticker := time.NewTicker(time.Second * timeToCheck)
 	isIdle := true
 	movePixel := 10
@@ -43,14 +47,17 @@ func moveMouse(comm chan struct{}, quit chan struct{}) {
 			isIdle = false
 			//fmt.Println("val received: ", isIdle)
 		case <-quit:
+			fmt.Println("stopped main app")
+			robotgo.StopEvent()
+			quitMouseClickHandler <- struct{}{}
 			return
 		}
 	}
 }
 
 func isPointerIdle(currentMousePos, lastMousePos *mousePos) bool {
-	fmt.Println("current : ", currentMousePos)
-	fmt.Println("last : ", lastMousePos)
+	// fmt.Println("current : ", currentMousePos)
+	// fmt.Println("last : ", lastMousePos)
 
 	if currentMousePos.mouseX == lastMousePos.mouseX &&
 		currentMousePos.mouseY == lastMousePos.mouseY {
@@ -67,33 +74,97 @@ func getMousePos() *mousePos {
 	}
 }
 
-func isMouseClicked(comm chan struct{}) {
+func isMouseClicked(comm, quit chan struct{}) {
 	go func() {
+		isRunning := true
 		for {
-			mleft := robotgo.AddEvent("mleft")
-			if mleft == 0 {
-				fmt.Println("mleft clicked")
-				time.Sleep(100 * time.Millisecond)
-				comm <- struct{}{}
+			select {
+			case <-quit:
+				fmt.Println("asked to quit")
+				isRunning = false
+				close(comm)
+			default:
+
+				fmt.Println("adding reg")
+				mleft := robotgo.AddEvent("mleft")
+				if mleft == 0 {
+					if isRunning {
+						fmt.Println("mleft clicked")
+						comm <- struct{}{}
+					} else {
+						return
+					}
+
+					time.Sleep(1000 * time.Millisecond)
+				}
+				//}
 			}
 		}
 	}()
 
-	go func() {
-		for {
-			mright := robotgo.AddEvent("mright")
-			if mright == 0 {
-				fmt.Println("mright clicked")
-				time.Sleep(100 * time.Millisecond)
-				comm <- struct{}{}
-			}
-		}
-	}()
+	//waiting for next robot go release in which
+	// we can handle multiple event registrations
+	// go func() {
+	// 	for {
+	// 		mright := robotgo.AddEvent("mright")
+	// 		if mright == 0 {
+	// 			fmt.Println("mright clicked")
+	// 			time.Sleep(100 * time.Millisecond)
+	// 			comm <- struct{}{}
+	// 		}
+	// 	}
+	// }()
 }
 
 func main() {
+	systray.Run(onReady, onExit)
+
+}
+func startApp() chan struct{} {
 	comm := make(chan struct{})
 	quit := make(chan struct{})
-	go isMouseClicked(comm)
-	moveMouse(comm, quit)
+	go moveMouse(comm, quit)
+	return quit
+}
+
+func onReady() {
+	go func() {
+		systray.SetIcon(icon.Data)
+		systray.SetTitle("AMM")
+		ammStart := systray.AddMenuItem("Start", "start the app")
+		ammPause := systray.AddMenuItem("Pause", "pause the app")
+		systray.AddSeparator()
+		mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
+		// Sets the icon of a menu item. Only available on Mac.
+		//mQuit.SetIcon(icon.Data)
+		var quit chan struct{}
+		for {
+			select {
+			case <-ammStart.ClickedCh:
+				fmt.Println("starting the app")
+				quit = startApp()
+
+			case <-ammPause.ClickedCh:
+				fmt.Println("pausing the app")
+				if quit != nil {
+					quit <- struct{}{}
+				} else {
+					fmt.Println("app is not started")
+				}
+
+			case <-mQuit.ClickedCh:
+				fmt.Println("Requesting quit")
+				systray.Quit()
+				fmt.Println("Finished quitting")
+				return
+			}
+		}
+
+	}()
+}
+
+func onExit() {
+	// clean up here
+	fmt.Println("exiting")
+
 }
