0d1828505b2356b370ddab301f24602eb5c9ea87
Prashant Gupta <prashantgupta24@gmail.com>
2019-03-13 16:57:03 -0700

    dev work
    * adding system tray feature
    
    * updated glide lock
    
    * working prototype
    
    * added notify package
    
    * refactoring packages
    
    * moved stuff out
    
    * adding lock file
    
    * changing activity function
    
    * moving stuff around
    
    * refactoring

---
+++ src/mousemover/mouseMover.go
@@ -0,0 +1,44 @@
+package mousemover
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-vgo/robotgo"
+	"github.com/prashantgupta24/activity-tracker/src/activity"
+	"github.com/prashantgupta24/activity-tracker/src/mouse"
+)
+
+func Start() (quit chan struct{}) {
+	quit = make(chan struct{})
+
+	activityTracker := &activity.ActivityTracker{
+		TimeToCheck: 5,
+	}
+
+	heartbeatCh, quitActivityTracker := activityTracker.Start()
+
+	go func() {
+		movePixel := 10
+		for {
+			select {
+			case heartbeat := <-heartbeatCh:
+				if !heartbeat.IsActivity {
+					currentMousePos := mouse.GetPosition()
+					fmt.Println("moving mouse at : ", time.Now())
+					nextMouseMov := &mouse.Position{
+						MouseX: currentMousePos.MouseX + movePixel,
+						MouseY: currentMousePos.MouseY + movePixel,
+					}
+					robotgo.Move(nextMouseMov.MouseX, nextMouseMov.MouseY)
+					movePixel *= -1
+				}
+			case <-quit:
+				fmt.Println("stopping mouse mover")
+				quitActivityTracker <- struct{}{}
+				return
+			}
+		}
+	}()
+	return quit
+}
